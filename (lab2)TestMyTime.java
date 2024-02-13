869d3dbafe27b8f810eedf82ce9a7af7ec75df15
Беседина Елизавета <125102825+BesedinaElizaveta@users.noreply.github.com>
2024-02-13 13:11:21 +1000

    Create (lab2)TestMyTime.java

---
+++ (lab2)TestMyTime.java
@@ -0,0 +1,8 @@
+public class TestMyTime{
+  public static void main(String[] args){
+    
+    MyTime mt1 = new MyTime();
+    MyTime mt2 = new MyTime(555550000);
+    MyTime mt3 = new MyTime(5, 23, 55);
+  }
+}
