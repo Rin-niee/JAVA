0eafc8f108b4acb54380ab63b21a384b69e46a18
Беседина Елизавета <125102825+BesedinaElizaveta@users.noreply.github.com>
2024-02-17 12:02:07 +1000

    Update (lab2)TestMyTime.java

--- (lab2)TestMyTime.java
+++ (lab2)TestMyTime.java
@@ -1,8 +1,12 @@
-public class TestMyTime{
-  public static void main(String[] args){
-    
-    MyTime mt1 = new MyTime();
-    MyTime mt2 = new MyTime(555550000);
-    MyTime mt3 = new MyTime(5, 23, 55);
+public class TestMyTime
+{
+  public static void main(String [] args)
+  {
+    MyTime t1= new MyTime();
+    MyTime t2 = new MyTime(555550000);
+    MyTime t3 = new MyTime(5, 23, 55);
+    System.out.println("Текущее время: " + t1);
+    System.out.println("Время с 00:00 1 января 1970г: "+ t2);
+    System.out.println("Указанное время: " + t3);
   }
 }
