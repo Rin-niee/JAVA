5e81fd0673ce3df32031c98aad5612680bfd49d3
Rin <125102825+Rin-niee@users.noreply.github.com>
2024-02-20 12:12:05 +1000

    Update and rename MyCharacter.java to (lab3)MyCharacter.java

---
+++ (lab3)MyCharacter.java
@@ -0,0 +1,10 @@
+/*Java-библиотека предоставляет класс Character. Предоставьте собственную
+реализацию следующих методов (назовите новый класс MyCharacter):
+  public MyCharacter(char value);
+  public char charValue();
+  public int compareTo(MyCharacter anotherCharacter);
+  public boolean equals(Object anotherCharacter);
+  public boolean isDigit();
+  public static boolean isDigit(MyCharacter ch);
+  public static boolean isLetter(MyCharacter ch);
+  public static boolean isLetterOrDigit(MyCharacter ch);*/
