2d7f5bf7a3dd29519063898b4cd3fbeb3f841c7c
Rin <125102825+Rin-niee@users.noreply.github.com>
2024-02-20 11:56:49 +1000

    Create (lab2)TestStopWatch.java

---
+++ (lab2)TestStopWatch.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+import java.util.Random;
+
+public class StopWatch
+{
+    private long startTime;
+    private long endTime;
+
+    public StopWatch()
+    {
+        reset();
+    }
+
+    public void reset()
+    {
+        startTime = System.currentTimeMillis();
+    }
+
+    public void start()
+    {
+        reset();
+    }
+
+    public void stop() {
+        endTime = System.currentTimeMillis();
+    }
+
+    public long getElapsedTime() {
+        return endTime - startTime;
+    }
+
+}
