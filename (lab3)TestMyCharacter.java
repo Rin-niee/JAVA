61f4c1b14a9c0dfe913db439882c0c498f01a567
Rin <125102825+Rin-niee@users.noreply.github.com>
2024-02-24 20:21:13 +1000

    Update (lab3)TestMyCharacter.java

--- (lab3)TestMyCharacter.java
+++ (lab3)TestMyCharacter.java
@@ -1,11 +1,29 @@
-public class TestMyCharacter
-{
-  public static void main(String [] args)
-  {
-    MyCharacter mc1 = new MyCharacter('r');
-    MyCharacter mc2 = new MyCharacter('k');
-    Object mc2 = new MyCharacter('k');
-    System.out.println("Compare To: " + mc1.compareTo(mc2));
-    System.out.println("Compare To: " + mc1.equals('b'));
-  }
+public class TestMyCharacter {
+    public static void main(String[] args) {
+        // Создание объекта MyCharacter
+        MyCharacter myCharacter = new MyCharacter('a');
+
+        // Вывод значения символа
+        System.out.println("Значение символа: " + myCharacter.charValue());
+
+        // Сравнение двух объектов MyCharacter
+        MyCharacter myCharacter2 = new MyCharacter('b');
+        System.out.println("Сравнение двух символов: " + myCharacter.compareTo(myCharacter2));
+
+        // Сравнение объекта MyCharacter и символа
+        char ch = 'c';
+        System.out.println("Сравнение символа и объекта MyCharacter: " + myCharacter.compareTo(ch));
+
+        // Проверка, является ли символ цифрой
+        System.out.println("Проверка, является ли символ цифрой: " + myCharacter.isDigit());
+
+        // Проверка, является ли символ цифрой (статический метод)
+        System.out.println("Проверка, является ли символ цифрой (статический метод): " + MyCharacter.isDigit(myCharacter));
+
+        // Проверка, является ли символ буквой (статический метод)
+        System.out.println("Проверка, является ли символ буквой (статический метод): " + MyCharacter.isLetter(myCharacter));
+
+        // Проверка, является ли символ буквой или цифрой (статический метод)
+        System.out.println("Проверка, является ли символ буквой или цифрой (статический метод): " + MyCharacter.isLetterOrDigit(myCharacter));
+    }
 }
