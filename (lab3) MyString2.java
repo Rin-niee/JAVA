a59178ada3b3f92a6a40ec2080cd62cb3bbd9aa3
Rin <125102825+Rin-niee@users.noreply.github.com>
2024-02-24 20:19:19 +1000

    Update (lab3) MyString2.java

--- (lab3) MyString2.java
+++ (lab3) MyString2.java
@@ -7,12 +7,12 @@ public boolean equals(Object obj);
 public int compareTo(MyString2 obj);
 public char[] toChars();
 public static MyString2 valueOf(boolean b);*/
+
+import java.util.Arrays;
+
 public class MyString2
 {
-  private char[] chars;
-  private int begin;
-  private int end;
-  private boolean b;
+  public char[] chars;
   
   public MyString2(char[] chars)
   {
@@ -20,30 +20,38 @@ public class MyString2
   }
   public MyString2 substring(int begin)
   {
-    
+    return new MyString2(Arrays.copyOfRange(chars, begin, chars.length));
+   
   }
   public MyString2 substring(int begin, int end)
   {
-    
+     return new MyString2(Arrays.copyOfRange(chars, begin, end));
   }
   public boolean equals(Object obj)
   {
-     if (obj instanceof MyString2) //если вместо класса MyCharacter другой объект, то 
-    {
-      return value == ((MyString2)obj).chars; //возвращает результат сравнения значений, класс anotherCharacter приводится к тому же классу, что и Character
-    }
-    return false;
+     if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        MyString2 other = (MyString2) obj;
+        return Arrays.equals(chars, other.chars);
   }
   public int compareTo(MyString2 obj)
   {
-    String.compareTo(chars, obj.value);
-  }
+    return String.valueOf(chars).compareTo(String.valueOf(obj.chars));
+    }
   public char[] toChars()
   {
-    
+      return chars;
   }
   public static MyString2 valueOf(boolean b)
   {
+    return new MyString2(String.valueOf(b).toCharArray());
     
   }
+  public String toString() {
+        return String.valueOf(chars);
+    }
 }
