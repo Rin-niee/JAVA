2ab558f30a2996f3902b7cda63ce8dc91291a63d
Rin <125102825+Rin-niee@users.noreply.github.com>
2024-02-27 12:50:25 +1000

    Update (lab3)TestMyString2.java

--- (lab3)TestMyString2.java
+++ (lab3)TestMyString2.java
@@ -4,6 +4,7 @@ public class TestMyString2 {
     public static void main(String[] args) {
         char[] chars = {'H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd'};
         char[] chars1 = {'O', 'k', 'a', 'y'};
+        
         MyString2 myString2 = new MyString2(chars);
         System.out.println("Исходная строка: " + myString2);
 
@@ -20,7 +21,7 @@ public class TestMyString2 {
         System.out.println("Сравнение строки и объекта MyString2: " + myString2.equals(str));
         
         char[] chars2 = myString2.toChars();
-        System.out.println("Преобразование объекта MyString2 в массив символов: " + Arrays.toString(chars2));
+        System.out.println("Преобразование объекта в массив символов: " + Arrays.toString(chars2));
 
         MyString2 myString2_3 = MyString2.valueOf(true);
         System.out.println("Создание объекта MyString2 из логического значения: " + myString2_3);
