5a8774bc7c44732e8ec818c4b20184c63881a90a
Rin <125102825+Rin-niee@users.noreply.github.com>
2024-03-05 21:30:50 +1000

    Update (lab4)Box.java

--- (lab4)Box.java
+++ (lab4)Box.java
@@ -2,6 +2,32 @@ import java.util.ArrayList;
 
 public class Box extends Shape
 {
-  private ArrayList<Shape> list = new ArrayList;
-  
+  private double volume;
+  private ArrayList<Shape> list = new ArrayList<>();
+   public Box(double volume) {
+        this.volume = volume;
+    }
+  public boolean add(Shape shape) {
+        if (shape instanceof SolidOfRevolution) {
+            double shapeVolume = ((SolidOfRevolution) shape).getVolume();
+            if (volume >= shapeVolume) {
+                shapesList.add(shape);
+                volume -= shapeVolume;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Box { volume: ").append(volume).append(", shapes: ");
+        for (Shape shape : shapesList) {
+            sb.append(shape.toString()).append(", ");
+        }
+        sb.delete(sb.length()-2, sb.length());
+        sb.append(" }");
+        return sb.toString();
+    }
 }
