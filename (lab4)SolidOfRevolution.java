b1dfb716ffe72c44e033772300171f42637e4e44
Rin <125102825+Rin-niee@users.noreply.github.com>
2024-03-12 13:01:25 +1000

    Update (lab4)SolidOfRevolution.java

--- (lab4)SolidOfRevolution.java
+++ (lab4)SolidOfRevolution.java
@@ -1,4 +1,19 @@
 public abstract class SolidOfRevolution extends Shape
 {
+  private java.util.Date dateCreated;
+  private double r;
+  public SolidOfRevolution()
+  {
+    dateCreated = new java.util.Date();
+  }
   
+  public String toString()
+  {
+    return "создан: " + dateCreated;
+  }
+  
+  public double getRadius()
+  {
+    return r;
+  }
 }
