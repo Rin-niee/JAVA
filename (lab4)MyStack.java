cfeaa171f5d379b631d6178ae547c290e8b5d0e6
Rin <125102825+Rin-niee@users.noreply.github.com>
2024-03-12 13:06:17 +1000

    Update (lab4)MyStack.java

--- (lab4)MyStack.java
+++ (lab4)MyStack.java
@@ -1,37 +1,80 @@
+// import java.util.ArrayList;
+
+// public class MyStack<T extends Comparable<T>> {
+//     private ArrayList<T> list = new ArrayList<>();
+
+//     public boolean isEmpty() {
+//         return list.isEmpty();
+//     }
+
+//     public int getSize() {
+//         return list.size();
+//     }
+
+//     public T peek() {
+//         return list.get(getSize() - 1);
+//     }
+
+//     public T pop() {
+//         T o = list.get(getSize() - 1);
+//         list.remove(getSize() - 1);
+//         return o;
+//     }
+
+//     public void push(T o) {
+//         list.add(o);
+//     }
+
+//     public void sort() {
+//         list.sort(Comparable::compareTo);
+//     }
+
+//     @Override
+//     /** Переопределяет метод toString класса Object */
+//     public String toString() {
+//         return "стек: " + list.toString();
+//     }
+// }
+
+
+
+
 import java.util.ArrayList;
 
-public class MyStack<T extends Comparable<T>> {
-    private ArrayList<T> list = new ArrayList<>();
-
-    public boolean isEmpty() {
-        return list.isEmpty();
-    }
-
-    public int getSize() {
-        return list.size();
-    }
-
-    public T peek() {
-        return list.get(getSize() - 1);
-    }
-
-    public T pop() {
-        T o = list.get(getSize() - 1);
-        list.remove(getSize() - 1);
-        return o;
-    }
-
-    public void push(T o) {
-        list.add(o);
-    }
-
-    public void sort() {
+public class MyStack
+{
+   private ArrayList<Object> list = new ArrayList<>();
+   public boolean isEmpty()
+   {
+    return list.isEmpty();
+   }
+   public int getSize()
+   {
+    return list.size();
+   }
+   public Object peek()
+   {
+    return list.get(getSize() - 1);
+   }
+   public Object pop()
+   {
+     Object o = list.get(getSize() - 1);
+     list.remove(getSize() - 1);
+     return o;
+   }
+   public void push(Object o)
+   {
+    list.add(o);
+   }
+   
+   public void sort()
+   {
         list.sort(Comparable::compareTo);
-    }
-
-    @Override
-    /** Переопределяет метод toString класса Object */
-    public String toString() {
-        return "стек: " + list.toString();
-    }
+  }
+  
+   @Override /** Переопределяет метод toString класса Object */
+   public String toString()
+   {
+    return "стек: " + list.toString();
+   }
 }
