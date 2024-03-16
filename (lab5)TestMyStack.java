45d70399504008ab6cd0d46479c3f15f929b2bb7
Rin <125102825+Rin-niee@users.noreply.github.com>
2024-03-16 20:03:10 +1000

    Create (lab5)TestMyStack.java

---
+++ (lab5)TestMyStack.java
@@ -0,0 +1,19 @@
+public class TestMySytack{
+    public static void main(String[] args){
+        MyStack stack1 = new MyStack();
+        stack1.push(1);
+        stack1.push(2);
+        stack1.push(3);
+
+        System.out.println("stack1: " + stack1.toString());
+
+        MyStack stack2 = (MyStack) stack1.clone();
+
+        System.out.println("stack1 isEmpty: " + stack1.isEmpty());
+        System.out.println("stack2 isEmpty: " + stack2.isEmpty());
+
+        stack1.pop();
+        System.out.println("stack1: " + stack1.toString());
+        System.out.println("stack2: " + stack2.toString());
+    }
+}
