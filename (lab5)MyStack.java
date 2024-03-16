cf5725a43fe0b7cef9a943750d392ea79d40a6c7
Rin <125102825+Rin-niee@users.noreply.github.com>
2024-03-16 20:02:35 +1000

    Create (lab5)MyStack.java

---
+++ (lab5)MyStack.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+
+public class MyStack
+{
+ private ArrayList< Object> list = new ArrayList<>();
+ public boolean isEmpty()
+ {
+  return list.isEmpty();
+ }
+ public int getSize()
+ {
+  return list.size();
+ }
+ public Object peek()
+ {
+  return list.get(getSize() - 1);
+ }
+ public Object pop()
+ {
+   Object o = list.get(getSize() - 1);
+   list.remove(getSize() - 1);
+   return o;
+ }
+ public void push(Object o)
+ {
+  list.add(o);
+ }
+ 
+ @Override /** Переопределяет метод toString класса Object */
+ public String toString() {
+ return "стек: " + list.toString();
+ }
+@Override
+  public Object clone()
+  {
+    try
+    {
+      MyStack cloned = (MyStack) super.clone();
+      cloned.list = (ArrayList<Object>) this.list.clone();
+        return cloned;
+    } catch (CloneNotSupportedException e)
+    {
+      return null; // в случае ошибки клонирования возвращаем null
+      }
+    }
+}
