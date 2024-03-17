1847b887cc55c0cc2a1b3d86ead38bee63be3f73
Rin <125102825+Rin-niee@users.noreply.github.com>
2024-03-17 18:23:58 +1000

    Update (lab5)MyStack.java

--- (lab5)MyStack.java
+++ (lab5)MyStack.java
@@ -1,46 +1,46 @@
 import java.util.ArrayList;
 
-public class MyStack
+public class MyStack implements Cloneable
 {
- private ArrayList< Object> list = new ArrayList<>();
- public boolean isEmpty()
- {
-  return list.isEmpty();
- }
- public int getSize()
- {
-  return list.size();
- }
- public Object peek()
- {
-  return list.get(getSize() - 1);
- }
- public Object pop()
- {
-   Object o = list.get(getSize() - 1);
-   list.remove(getSize() - 1);
-   return o;
- }
- public void push(Object o)
- {
-  list.add(o);
- }
- 
- @Override /** Переопределяет метод toString класса Object */
- public String toString() {
- return "стек: " + list.toString();
- }
-@Override
-  public Object clone()
-  {
-    try
+   private ArrayList< Object> list = new ArrayList<>();
+   public boolean isEmpty()
+   {
+    return list.isEmpty();
+   }
+   public int getSize()
+   {
+    return list.size();
+   }
+   public Object peek()
+   {
+    return list.get(getSize() - 1);
+   }
+   public Object pop()
+   {
+     Object o = list.get(getSize() - 1);
+     list.remove(getSize() - 1);
+     return o;
+   }
+   public void push(Object o)
+   {
+    list.add(o);
+   }
+   
+   @Override /** Переопределяет метод toString класса Object */
+   public String toString() {
+   return "стек: " + list.toString();
+   }
+  @Override
+    public Object clone()
     {
-      MyStack cloned = (MyStack) super.clone();
-      cloned.list = (ArrayList<Object>) this.list.clone();
-        return cloned;
-    } catch (CloneNotSupportedException e)
-    {
-      return null; // в случае ошибки клонирования возвращаем null
+      try
+      {
+        MyStack c = (MyStack) super.clone();
+        c.list = (ArrayList<Object>) this.list.clone();
+          return c;
+      } catch (CloneNotSupportedException ex)
+      {
+        return null;
       }
     }
 }
