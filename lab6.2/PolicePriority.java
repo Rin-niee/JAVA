99adfec960884788a5e23acb0940cf932f5c99c3
Rin <125102825+Rin-niee@users.noreply.github.com>
2024-03-19 12:53:52 +1000

    Create PolicePriority.java

---
+++ lab6.2/PolicePriority.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.util.PriorityQueue;
+
+public class PolicePriority implements Comparator<Car>
+{
+    public int compare(Car car1, Car car2)
+    {
+        if (car1.getCarType().equals("Police") && !car2.getCarType().equals("Police"))
+        {
+            return -1; // Car1 has higher priority
+        } 
+        else if (!car1.getCarType().equals("Police") && car2.getCarType().equals("Medical"))
+        {
+            return 1; // Car2 has higher priority
+        }
+        else
+        {
+            return 0; // Equal priority
+        }
+    }
+}
