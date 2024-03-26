92b90597e5b39d30bb28823c4d76acd5d0ac0dd8
Rin <125102825+Rin-niee@users.noreply.github.com>
2024-03-26 12:51:10 +1000

    Update TestGenericStack1.java

--- lab7/TestGenericStack1.java
+++ lab7/TestGenericStack1.java
@@ -4,21 +4,23 @@ import java.util.Stack;
 
 public class TestGenericStack 
 {
-  public static void main(String[] args) {
+  public static void main(String[] args)
+  {
         // Пример использования
         GenericStack<Integer> stack = new GenericStack<>();
         ArrayList<Integer> list = new ArrayList<>();
         list.add(1);
         list.add(2);
         list.add(3);
-        list.add(2); // Дубликат
+        list.add(2);
         list.add(4);
-        list.add(3); // Дубликат
+        list.add(3);
 
         stack.addWithoutDuplicates(list);
 
         // Вывод стека после добавления без дубликатов
-        while (!stack.isEmpty()) {
+        while (!stack.isEmpty())
+        {
             System.out.println(stack.pop());
         }
     }
