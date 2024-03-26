c5a4508a97af8222fb876e789a0a7a3b018a524e
Rin <125102825+Rin-niee@users.noreply.github.com>
2024-03-26 12:50:45 +1000

    Update GenericStack1.java

--- lab7/GenericStack1.java
+++ lab7/GenericStack1.java
@@ -1,53 +1,69 @@
+/*Возьмите класс-дженерик класс GenericStack из варианта 1. Напишите метод,
+которому передается коллекция объектов типа ArrayList и добавляется в
+конец стека, но так, чтобы отбрасывались значения-дубликаты. Для поиска и
+удаления дубликатов необходимо использовать метод contains() интерфейса
+List.*/
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Stack;
 
-public class GenericStack1<E> 
+public class GenericStack<E>
 {
-  private Stack<E> stack;
-
-    public GenericStack()
+   private java.util.ArrayList<E> list = new java.util.ArrayList<>();
+   private  java.util.Stack<E> stack = new java.util.Stack<>();
+   //новый стек
+   public GenericStack()
     {
         stack = new Stack<>();
     }
-  
-   private java.util.ArrayList<E> list = new java.util.ArrayList<>();
-   public int getSize() 
+    
+   //получить размер стека
+   public int getSize()
    {
-    return list.size();
-   }
-   public E peek()
-  {
-    return list.get(getSize() - 1);
-   }
-   public void push(E o)
-   {
-    list.add(o);
+      return list.size();
    }
    
+   //узнать что находится в конце стека
+   public E peek()
+   {
+      return list.get(getSize() - 1);
+   }
+   //добавить в стек число
+   public void push(E o)
+   {
+      list.add(o);
+   }
+   
+   //вывести и удалить число в стеке
    public E pop()
    {
      E o = list.get(getSize() - 1);
      list.remove(getSize() - 1);
      return o;
    }
+   
+   //проверка пустой ли стек
    public boolean isEmpty()
    {
-    return list.isEmpty();
+      return list.isEmpty();
    }
-   public void addWithoutDuplicates(ArrayList<E> elements) {
+   //по плану сортировка
+   public void addWithoutDuplicates(ArrayList<E> elements)
+   {
+     
         for (E element : elements)
         {
-            if (!stack.contains(element))
+            if (!list.contains(element))
             {
-                stack.push(element);
+                list.add(element);
             }
         }
     }
   
+  //стринг ту стринг
    @Override
-   public String toString()
-   {
-    return "стек: " + list.toString();
+   public String toString(){
+   return "стек: " + list.toString();
    }
 }
