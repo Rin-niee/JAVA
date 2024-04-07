67128c96d45fb22a8fbebaea7e87d216c1fa74aa
Rin <125102825+Rin-niee@users.noreply.github.com>
2024-04-07 21:58:06 +1000

    Update TestCoolClass.java

--- lab8/TestCoolClass.java
+++ lab8/TestCoolClass.java
@@ -1,7 +1,50 @@
+import java.util.Arrays;
+
 public class TestCoolClass
 {
-public void main(String[] args)
-  {
-    
-  }
+    public static void main(String[] args)
+    {
+        CoolClass coolClass = new CoolClass();
+
+        // Добавление элементов
+        try {
+            coolClass.add(1);
+            coolClass.add(2);
+            coolClass.add(3);
+            coolClass.add(4);
+            coolClass.add(5);
+            coolClass.add(1);
+            coolClass.add(2);
+            coolClass.add(3);
+            coolClass.add(4);
+            coolClass.add(5);
+            coolClass.add(5);
+        } catch (ContainerFullException e){
+            System.out.println("Контейнер заполнен");
+        }
+
+        // Удаление элементов
+        try {
+            System.out.println("Удаленные элементы: " + coolClass.remove());
+            System.out.println("Удаленные элементы: " + coolClass.remove());
+            System.out.println("Удаленные элементы: " + coolClass.remove());
+        } catch (ContainerEmptyException e){
+            System.out.println("Контейнер пуст");
+        }
+        try
+        {
+            coolClass.sort();
+        }
+        catch (AllElementsAreEqualException e)
+        {
+          System.out.println("Одинаковые элементы");
+        }
+        try {
+          coolClass.search(1);
+        } catch (MultipleElementsFoundException e){
+          System.out.println("что-то не так с элементами");
+        }
+        // Печать оставшихся элементов
+        System.out.println("Оставшиеся элементы: " + coolClass);
+    }
 }
